0943c39afbc6a5b6aa20f19736bff889df8479ed
Paulo Vinicius Cristaldo A <102568081+Paulo-vca@users.noreply.github.com>
2023-08-21 22:26:23 -0400

    TEST

---
+++ backbeto/app.js
@@ -0,0 +1,55 @@
+// config do express
+const express = require("express");
+
+const productController = require('./controller/product/productController')
+
+const app = express();
+const port = 3000;
+
+const productRouter = express.Router()
+
+// deconding / enconding
+app.use(express.urlencoded({ extended: true }));
+
+productRouter
+    .route('/api/product')
+    .get(productController.getOne)
+    .post(productController.addProduct)
+    
+productRouter
+    .route('/api/product/:id')
+    .get(productController.getAll)
+    .put(productController.updateProduct)
+    .delete(productController.deleteProduct)
+    
+
+app.use(productRouter)
+
+
+
+
+
+
+
+
+// app.get('/api/product', productController.getAll)
+// app.get('/api/product/:id', productController.getOne)
+// app.post('/api/product', productController.addProduct)
+// app.put('/api/product/:id', productController.updateProduct)
+// app.delete('/api/product/:id', productController.deleteProduct)
+
+
+app.get("/", (req, res) => {
+  res.send("Meu Express está rodando...");
+});
+
+
+
+
+
+/**
+ * Configuração da porta
+ */
+app.listen(port, () => {
+  console.log(`Express is running on port ${port}`);
+});
---
+++ backbeto/data/userData.js
@@ -0,0 +1,18 @@
+exports.products = [
+    {
+      id: 0,
+      name: "user 1",
+      email: "user@email.com"
+    },
+    {
+      id: 1,
+      name: "user 2",
+      email: "user4@email.com"
+    },
+    {
+      id: 2,
+      name: "Product 3",
+      email: "user3@email.com"
+    },
+  ];
+  
